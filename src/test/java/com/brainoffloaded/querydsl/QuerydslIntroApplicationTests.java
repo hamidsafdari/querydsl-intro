3aa8466e72fbd5c37945de6bdf5f92ee017d6712
Hamid Safdari <hamidsafdari22@gmail.com>
2023-07-08 09:20:39 +0500

    initial commit

---
+++ src/test/java/com/brainoffloaded/querydsl/QuerydslIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brainoffloaded.querydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerydslIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
