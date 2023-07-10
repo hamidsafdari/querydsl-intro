8f5deeb7695c27d8521fbf9f17982c27ff86e7fd
Hamid Safdari <hamidsafdari22@gmail.com>
2023-07-10 22:25:06 +0500

    feat: querydsl-intro, java 17, gradle

--- src/main/java/com/brainoffloaded/querydsl/entity/Fruit.java
+++ src/main/java/com/brainoffloaded/querydsl/entity/Fruit.java
@@ -1,15 +1,14 @@
 package com.brainoffloaded.querydsl.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 @Data
 @Entity
